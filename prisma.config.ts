1966d619d47ee9dde1e3528d3974b39941f20443
SantyPB19 <santiago.alvarez165@pascualbravo.edu.co>
2026-04-07 07:13:20 -0500

    actualizacion 07/04/2026

---
+++ prisma.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'prisma/config'
+import * as dotenv from 'dotenv'
+
+dotenv.config({ path: '.env' })
+
+export default defineConfig({
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
+})
